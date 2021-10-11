a081dab07f95251fe19bdb8ebc11edfbbf06fc45
rensortino <rensortino@gmail.com>
2021-10-11 17:46:21 +0200

    Added PNG conversion

--- split_dataset.py
+++ split_dataset.py
@@ -1,10 +1,24 @@
 import math
+from PIL import Image 
 from pathlib import Path
+from astropy.io import fits
 import random
+import torch
+from torchvision.utils import save_image
 from shutil import copyfile
+from astropy.visualization import ZScaleInterval
 import argparse
 import json
 from tqdm import tqdm
+
+def fits_to_png(img, contrast=0.15):
+    
+    interval = ZScaleInterval(contrast=contrast)
+    min, max = interval.get_limits(img)
+
+    img = (img - min) / (max - min)
+
+    return img
 
 def copy_annotations(img_name, sample_folder, masks_path, dst_json_folder):
     for json_obj_file in masks_path.glob(f'mask_{img_name}.json'):
@@ -38,8 +52,48 @@ def copy_masks(img_name, dst_mask_folder, masks_path):
 
     for mask_obj_file in masks_path.glob(f'*mask_{img_name}*.fits'):
         dst_mask_path = dst_mask_folder / f'{sample_folder.stem}_{mask_obj_file.stem}{mask_obj_file.suffix}' # splits/{phase}/masks/sample1_mask_galaxy0001_obj1.fits
-        copyfile(mask_obj_file, dst_mask_path)
 
+        dst_mask_png_folder = dst_mask_folder.parent / (dst_mask_folder.name + '_png')
+        dst_mask_png_path = dst_mask_png_folder / f'{sample_folder.stem}_{mask_obj_file.stem}.png' # splits/{phase}/masks_png/sample1_mask_galaxy0001_obj1.fits
+        
+        copyfile(mask_obj_file, dst_mask_path)
+        convert_and_save(mask_obj_file, dst_mask_png_path)
+
+def create_img_dirs(output_path, split):
+    dst_folder = output_path / split
+    dst_img_folder = dst_folder / 'imgs'
+    dst_img_png_folder = dst_folder / 'imgs_png'
+
+    dst_folder.mkdir(exist_ok=True)
+    dst_img_folder.mkdir(exist_ok=True)
+    dst_img_png_folder.mkdir(exist_ok=True)
+
+    return dst_img_folder, dst_img_png_folder
+
+def create_mask_dirs(output_path, split):
+    dst_folder = output_path / split
+    dst_mask_folder = dst_folder / 'masks'
+    dst_mask_png_folder = dst_folder / 'masks_png'
+
+    dst_folder.mkdir(exist_ok=True)
+    dst_mask_folder.mkdir(exist_ok=True)
+    dst_mask_png_folder.mkdir(exist_ok=True)
+
+    return dst_mask_folder, dst_mask_png_folder
+
+def create_annotation_dirs(output_path, split):
+    dst_folder = output_path / split
+    dst_json_folder = dst_folder / 'annotations'
+
+    dst_folder.mkdir(exist_ok=True)
+    dst_json_folder.mkdir(exist_ok=True)
+
+    return dst_json_folder
+
+def convert_and_save(fits_path, dst_png_path):
+    fits_img = fits.getdata(fits_path)
+    png_img = fits_to_png(fits_img)
+    save_image(torch.from_numpy(png_img), dst_png_path)
 
 if __name__ == '__main__':
 
@@ -58,39 +112,19 @@ if __name__ == '__main__':
     test_perc = 0.2
 
     output_path = Path(args.out_path)
-
-    dst_train_folder = output_path / 'train'
-    dst_img_train_folder = output_path / 'train' / 'imgs'
-    dst_mask_train_folder = output_path / 'train' / 'masks'
-    dst_json_train_folder = output_path / 'train' / 'annotations'
-
-    dst_val_folder = output_path / 'val'
-    dst_img_val_folder = output_path / 'val' / 'imgs'
-    dst_mask_val_folder = output_path / 'val' / 'masks'
-    dst_json_val_folder = output_path / 'val' / 'annotations'
-
-    dst_test_folder = output_path / 'test'
-    dst_img_test_folder = output_path / 'test' / 'imgs'
-    dst_mask_test_folder = output_path / 'test' / 'masks'
-    dst_json_test_folder = output_path / 'test' / 'annotations'
-
     output_path.mkdir(exist_ok=True)
 
-    dst_train_folder.mkdir(exist_ok=True)
-    dst_img_train_folder.mkdir(exist_ok=True)
-    dst_mask_train_folder.mkdir(exist_ok=True)
-    dst_json_train_folder.mkdir(exist_ok=True)
+    dst_img_train_folder, dst_png_train_folder = create_img_dirs(output_path, 'train')
+    dst_mask_train_folder, dst_mask_png_train_folder = create_mask_dirs(output_path, 'train')
+    dst_json_train_folder = create_annotation_dirs(output_path, 'train')
 
-    dst_val_folder.mkdir(exist_ok=True)
-    dst_img_val_folder.mkdir(exist_ok=True)
-    dst_mask_val_folder.mkdir(exist_ok=True)
-    dst_json_val_folder.mkdir(exist_ok=True)
+    dst_img_val_folder, dst_png_val_folder = create_img_dirs(output_path, 'val')
+    dst_mask_val_folder, dst_mask_png_val_folder = create_mask_dirs(output_path, 'val')
+    dst_json_val_folder = create_annotation_dirs(output_path, 'val')
 
-    dst_test_folder.mkdir(exist_ok=True)
-    dst_img_test_folder.mkdir(exist_ok=True)
-    dst_mask_test_folder.mkdir(exist_ok=True)
-    dst_json_test_folder.mkdir(exist_ok=True)
-
+    dst_img_test_folder, dst_png_test_folder = create_img_dirs(output_path, 'test')
+    dst_mask_test_folder, dst_mask_png_test_folder = create_mask_dirs(output_path, 'test')
+    dst_json_test_folder = create_annotation_dirs(output_path, 'test')
 
     for class_folder in data_path.glob('*'):
 
@@ -124,10 +158,13 @@ if __name__ == '__main__':
                     t.set_description(f'Train split {class_folder.stem} - {sample_folder.stem}')
                     img_name = filename.stem # galaxy0001
                     dst_img_path = dst_img_train_folder / f'{sample_folder.stem}_{filename.stem}{filename.suffix}' # splits/train/imgs/sample1_galaxy0001.fits
+                    dst_png_path = dst_png_train_folder / f'{sample_folder.stem}_{filename.stem}.png' # splits/train/png_imgs/sample1_galaxy0001.fits
 
                     copy_annotations(img_name, sample_folder, masks_path, dst_json_train_folder)
                     copy_masks(img_name, dst_mask_train_folder, masks_path)
                     copyfile(filename, dst_img_path)
+
+                    convert_and_save(filename, dst_png_path)
             
             with tqdm(val_split) as t:
 
@@ -136,10 +173,14 @@ if __name__ == '__main__':
 
                     img_name = filename.stem # galaxy0001
                     dst_img_path = dst_img_val_folder / f'{sample_folder.stem}_{filename.stem}{filename.suffix}' # splits/val/imgs/sample1_galaxy0001.fits
+                    dst_png_path = dst_png_val_folder / f'{sample_folder.stem}_{filename.stem}.png' # splits/val/imgs/sample1_galaxy0001.fits
 
                     copy_annotations(img_name, sample_folder, masks_path, dst_json_val_folder)
                     copy_masks(img_name, dst_mask_val_folder, masks_path)
                     copyfile(filename, dst_img_path)
+
+                    convert_and_save(filename, dst_png_path)
+
 
             with tqdm(test_split) as t:
 
@@ -148,12 +189,10 @@ if __name__ == '__main__':
 
                     img_name = filename.stem # galaxy0001
                     dst_img_path = dst_img_test_folder / f'{sample_folder.stem}_{filename.stem}{filename.suffix}' # splits/test/imgs/sample1_galaxy0001.fits
+                    dst_png_path = dst_png_test_folder / f'{sample_folder.stem}_{filename.stem}.png' # splits/test/imgs/sample1_galaxy0001.fits
 
                     copy_annotations(img_name, sample_folder, masks_path, dst_json_test_folder)
                     copy_masks(img_name, dst_mask_test_folder, masks_path)
                     copyfile(filename, dst_img_path)
 
-
-
-
-
+                    convert_and_save(filename, dst_png_path)
