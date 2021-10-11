2356b6741cab141219d68b8d896668c44ce04ccb
rensortino <rensortino@gmail.com>
2021-10-11 16:40:42 +0200

    Fixed naming in annotations

--- split_dataset.py
+++ split_dataset.py
@@ -6,23 +6,31 @@ import argparse
 import json
 from tqdm import tqdm
 
-
-def copy_annotations(filename, dst_img_path, dst_mask_folder, dst_json_folder, masks_path):
-    
+def copy_annotations(img_name, sample_folder, masks_path, dst_json_folder):
     for json_obj_file in masks_path.glob(f'mask_{img_name}.json'):
         dst_json_path = dst_json_folder / f'{sample_folder.stem}_{json_obj_file.stem}{json_obj_file.suffix}' # splits/{phase}/sample1_mask_galaxy0001.json
-        with open(txt, 'a') as out:
-            out.write(f'{json_obj_file} to {dst_json_path}\n')
 
         with open(json_obj_file) as j:
             data = json.load(j)
+            img_name = Path(data['img'].split('/')[-1])
+            renamed_img_name = f'{sample_folder.stem}_{img_name}'
+            data['img'] = data['img'].replace(str(img_name), renamed_img_name)
             objs = data['objs']
+            renamed_objs = []
             for obj in objs:
                 if not obj['class']:
                     with open('errors.txt', 'a') as o:
                         o.write(f'Mask {obj["mask"]} has no class\n')
-            
-        copyfile(json_obj_file, dst_json_path)
+
+                renamed_mask = f'{sample_folder.stem}_{obj["mask"]}'
+                obj['mask'] = renamed_mask
+                renamed_objs.append(obj)
+
+        with open(dst_json_path, 'w') as out:
+            json.dump({'img': renamed_img_name, 'objs': renamed_objs}, out, indent=2)
+        # copyfile(json_obj_file, dst_json_path)
+
+def copy_masks(img_name, dst_mask_folder, masks_path):
 
     if not masks_path.glob(f'*mask_{img_name}*.fits'):
         with open('errors.txt', 'a') as o:
@@ -30,13 +38,7 @@ def copy_annotations(filename, dst_img_path, dst_mask_folder, dst_json_folder, m
 
     for mask_obj_file in masks_path.glob(f'*mask_{img_name}*.fits'):
         dst_mask_path = dst_mask_folder / f'{sample_folder.stem}_{mask_obj_file.stem}{mask_obj_file.suffix}' # splits/{phase}/masks/sample1_mask_galaxy0001_obj1.fits
-        with open(txt, 'a') as out:
-            out.write(f'{mask_obj_file} to {dst_mask_path}\n')
         copyfile(mask_obj_file, dst_mask_path)
-    
-    with open(txt, 'a') as out:
-        out.write(f'{filename} to {dst_img_path}\n')
-    copyfile(filename, dst_img_path)
 
 
 if __name__ == '__main__':
@@ -89,10 +91,6 @@ if __name__ == '__main__':
     dst_mask_test_folder.mkdir(exist_ok=True)
     dst_json_test_folder.mkdir(exist_ok=True)
 
-    txt = Path('copies.txt')
-
-    if txt.exists():
-        txt.unlink()
 
     for class_folder in data_path.glob('*'):
 
@@ -127,7 +125,9 @@ if __name__ == '__main__':
                     img_name = filename.stem # galaxy0001
                     dst_img_path = dst_img_train_folder / f'{sample_folder.stem}_{filename.stem}{filename.suffix}' # splits/train/imgs/sample1_galaxy0001.fits
 
-                    copy_annotations(filename, dst_img_path, dst_mask_train_folder, dst_json_train_folder, masks_path)
+                    copy_annotations(img_name, sample_folder, masks_path, dst_json_train_folder)
+                    copy_masks(img_name, dst_mask_train_folder, masks_path)
+                    copyfile(filename, dst_img_path)
             
             with tqdm(val_split) as t:
 
@@ -137,7 +137,9 @@ if __name__ == '__main__':
                     img_name = filename.stem # galaxy0001
                     dst_img_path = dst_img_val_folder / f'{sample_folder.stem}_{filename.stem}{filename.suffix}' # splits/val/imgs/sample1_galaxy0001.fits
 
-                    copy_annotations(filename, dst_img_path, dst_mask_val_folder, dst_json_val_folder, masks_path)
+                    copy_annotations(img_name, sample_folder, masks_path, dst_json_val_folder)
+                    copy_masks(img_name, dst_mask_val_folder, masks_path)
+                    copyfile(filename, dst_img_path)
 
             with tqdm(test_split) as t:
 
@@ -147,7 +149,9 @@ if __name__ == '__main__':
                     img_name = filename.stem # galaxy0001
                     dst_img_path = dst_img_test_folder / f'{sample_folder.stem}_{filename.stem}{filename.suffix}' # splits/test/imgs/sample1_galaxy0001.fits
 
-                    copy_annotations(filename, dst_img_path, dst_mask_test_folder, dst_json_test_folder, masks_path)
+                    copy_annotations(img_name, sample_folder, masks_path, dst_json_test_folder)
+                    copy_masks(img_name, dst_mask_test_folder, masks_path)
+                    copyfile(filename, dst_img_path)
 
 
 
